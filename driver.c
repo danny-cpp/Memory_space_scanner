1737586240cfe1e98b7cb870fd2123103b2cd4c2
Danh Nguyen <huudanh2908@gmail.com>
2021-01-28 23:56:54 -0500

    malloc

--- driver.c
+++ driver.c
@@ -1,14 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "memlayout.h"
 
 int main() {
-    printf("Hi\n");
-    struct memregion results[20];
-    int status = get_mem_layout(results, 20);
+    unsigned int array_size = 10;
 
-    for (unsigned int i = 0; i < 20; i++) {
+    printf("Memory before massive malloc:\n");
+    struct memregion results[array_size];
+
+    int status = get_mem_layout(results, array_size);
+    for (unsigned int i = 0; i < array_size; i++) {
         print_memregion(results[i]);
     }
+
+    printf("\nMemory after massive malloc:\n");
+
+    int* d = (int*)malloc(1000000*sizeof(int));
+    for (unsigned long i = 0; i < 1000000; i++) {
+        d[i] = 1;
+    }
+
+
+    get_mem_layout(results, array_size);
+    for (unsigned int i = 0; i < array_size; i++) {
+        print_memregion(results[i]);
+    }
+
 }
 
 
